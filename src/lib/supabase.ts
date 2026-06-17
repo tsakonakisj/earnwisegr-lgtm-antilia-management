fdf3bef754b0deba158465b0cf515fb8af830e3d
tsakonakisj <tsakonakisj@gmail.com>
2026-06-17 13:44:43 +0300

    Update client to use service role key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qlmmbudwpttjtsslrvqm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsbW1idWR3cHR0anRzc2xydnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxOTI3ODEsImV4cCI6MjA5Mzc2ODc4MX0.gAb_osLSuySNRvizE-lCQf6t527Hd3UWtPVl-kCU6xg';
+const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsbW1idWR3cHR0anRzc2xydnFtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODE5Mjc4MSwiZXhwIjoyMDkzNzY4NzgxfQ.ftDfpzBJm7SkSAlJqiur0zImlZxkaUrqcUInHdI7XXk';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
 export const isDemoMode = false;
